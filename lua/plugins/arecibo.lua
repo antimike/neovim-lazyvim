2e31b4c3eb7967e43decb5ecf74eee7eea09c0b7
Michael Haynes <antihaynes@gmail.com>
2023-03-05 17:57:44 -0600

    Disable arecibo and mdeval

--- lua/plugins/arecibo.lua
+++ lua/plugins/arecibo.lua
@@ -1,4 +1,5 @@
 return {
+  enabled = false,
   "nvim-telescope/telescope-arecibo.nvim",
   rocks = { "openssl", "lua-http-parser" },
 }
--- lua/plugins/mdeval.lua
+++ lua/plugins/mdeval.lua
@@ -1,4 +1,5 @@
 return {
+  enabled = false,
   "jubnzv/mdeval.nvim",
   ft = {
     "org",
