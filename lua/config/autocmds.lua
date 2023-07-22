5da83a64e69fc52b1e545991ad68d3cf5317c892
Michael Haynes <antimike@gmail.com>
2023-07-21 20:28:40 -0500

    Add ghost-nvim stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.cmd([[
+augroup nvim_ghost_user_autocommands
+  au User www.reddit.com,www.stackoverflow.com setfiletype markdown
+  au User www.reddit.com,www.github.com setfiletype markdown
+  au User *github.com setfiletype markdown
+augroup END
+]])
